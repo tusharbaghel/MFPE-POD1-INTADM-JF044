ef0dc29630c090cf48c25ae1301ad306bf0acdc2
tbagh <tusharbaghel706@gmail.com>
2022-07-20 10:08:43 +0530

    Module section removed from parameters

--- src/main/java/com/S3LT/fleetmsv2/parameters/repositories/ModuleRepository.java
+++
@@ -1,4 +0,0 @@
-package com.S3LT.fleetmsv2.parameters.repositories;
-
-public interface ModuleRepository {
-}
--- src/main/java/com/S3LT/fleetmsv2/parameters/services/ModuleService.java
+++
@@ -1,4 +0,0 @@
-package com.S3LT.fleetmsv2.parameters.services;
-
-public class ModuleService {
-}
